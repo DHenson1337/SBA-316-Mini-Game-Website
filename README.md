7b4c14c34b2b35798dd130ce010b4707b9519db5
dhenson <davonhensontech@gmail.com>
2024-11-09 10:53:16 -0500

    Added a second pathway to choices

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # SBA-316-Mini-Game-Website
+
+HI!
+
+This repo contains my attempt at making my first set of interactive MiniGames
+utilizing HTML, CSS and Javascript
+
+It features games such as
+
+1. PunI-shment mountain:
+   A text based adventure with lots of puns and a hint of cringe, sure to bring smiles to all
+   pun lovers and secret pun lovers. (You know who you are :P)
+
+2. Game Pending:
+
+3. Game Pending:
+
+4. News Letter(Pending):
+   This is my attempt at creating Validation. . .
+   No its not that I need validation. Am happy about myself...On most days......at least 1/2 the time.
+   Enough about that!
+   This was simply a requirment for my project!
+   But if you want to give me positive Validation I wouldn't be opposed~
+
+   5. A Spooky toogle switch. (Pending)
+      Something Spoooooky, will happen if clicked, or not
+      depends on how much i've really understand Javascript up to this point heh.
--- scripts/punIshment-mountain.js
+++ scripts/punIshment-mountain.js
@@ -31,28 +31,84 @@ function showTextNode(textNodeIndex) {
 
 //Shows relevant options on screen
 function showOption(option) {
-  return true;
+  //returns options without a required state and options that pass the required(state) perameter
+  return option.requiredState == null || option.requiredState(state);
 }
 
 //Selects the options
-function selectOption(option) {}
+function selectOption(option) {
+  const nextTextNodeId = option.nextText;
+  state = Object.assign(state, option.setState);
+  showTextNode(nextTextNodeId);
+}
 
 // The game choices and Pathways
 const textNodes = [
   {
     id: 1,
-    text: "You play as Puni, with their uniqe powah of Pun-Ishment, your goal is to reach the top of the summit while facing various challanges Good Luck!",
+    text: "You play as Puni, with their uniqe powah of word Pun-Ishment, your goal is to reach the top of the summit while facing various challanges Good Luck!",
     options: [
       {
         text: "Together we Peak!",
         setState: { punPass: true },
         nextText: 2, //Goes to the next option AKA option 2
       },
-      { text: "...You've hit rock bottom", nextText: 2 },
+      { text: "...No, am not doing this", nextText: 2 },
     ],
   },
   {
     id: 2,
+    text: "The next step in the adventure: Blah, Blah, Blah, Blah, blah, blah, blah, s",
+    options: [
+      {
+        text: "User button click option if they choose they peak",
+        requiredState: (currentState) => currentState.punPass,
+        setState: { punPass: false, powah: true },
+        nextText: 3,
+      },
+      {
+        text: "User button click option if they choose they peak and prefer rhymes",
+        requiredState: (currentState) => currentState.punPass,
+        setState: { punPass: false, rhyme: true },
+        nextText: 3,
+      },
+      {
+        text: "Am not playing this dumb game!!!",
+        setState: { loser: true },
+        nextText: 3,
+      },
+      {
+        text: "Fine i'll give it a chance, but no word games!",
+        setState: { powerLess: true },
+        nextText: 3,
+      },
+    ],
+  },
+  {
+    id: 3,
+    text: "The next step in the adventure: Blah, Blah, Blah, Blah, blah, blah, blah, s",
+    options: [
+      {
+        text: "Text about rhyme or pun adv continuted",
+        requiredState: (currentState) =>
+          currentState.powah || currentState.rhyme,
+
+        nextText: 4,
+      },
+      {
+        text: "Text about rhyme or pun adv continued",
+        requiredState: (currentState) =>
+          currentState.powah || currentState.rhyme,
+
+        nextText: 4,
+      },
+      {
+        text: "Neglecting the powers invested within you, you continue your adventure as a mere mortal \n Amongst...not gods but other mortals with powers. \n Listen, i'll be honest, this isn't going to be eazy!",
+        requiredState: (currentState) => currentState.powerLess,
+
+        nextText: 4,
+      },
+    ],
   },
 ];
 //Calls the start Game Function
